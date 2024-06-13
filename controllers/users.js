7369bf11a6ca7164acfff832ca8e9821f9966eb9
Wr3tchedTorch <ericmoura511@gmail.com>
2024-06-12 21:35:25 -0300

    api for adding new users being implemented

--- controllers/users.js
+++ controllers/users.js
@@ -1,16 +1,28 @@
 const express = require("express");
 const User = require("../models/user");
+const bcrypt = require("bcrypt");
 const userRouter = express.Router();
 
 userRouter
   .get("/", async (req, res) => {
     const users = await User.find({});
-    console.log(users);
     res.status(200).json(users);
   })
   .get("/topFive", async (req, res) => {
     const users = await User.find({}).sort("score");    
     res.status(200).json(users.slice(Math.max(users.length-5, 0)));
+  })
+  .post("/", async (req, res) => {
+    const body = req.body;
+    const passwordHash = await bcrypt.hash(body.password, 10);
+
+    const user = new User({
+      username: body.username,
+      password: passwordHash,
+      score: body.score | 0
+    });
+    await user.save();
+    res.status(201).json(user);
   });
 
 module.exports = userRouter;
--- tests/user_api.test.js
+++ tests/user_api.test.js
@@ -15,16 +15,45 @@ beforeEach(async () => {
 describe("testing the users api", () => {
   describe("geting users", () => {
     test("get returns the correct number of users", async () => {
-      const result = await api.get("/api/users").expect(200).expect("Content-Type", /application\/json/);
-      console.log(result.body);
+      const result = await api
+        .get("/api/users")
+        .expect(200)
+        .expect("Content-Type", /application\/json/);
+
       assert.strictEqual(result.body.length, helper.initialUsers.length);
     });
   
     test("get returns users with highest score", async () => {
-      const result = await api.get("/api/users/topFive").expect(200).expect("Content-Type", /application\/json/);      
+      const result = await api
+        .get("/api/users/topFive")
+        .expect(200)
+        .expect("Content-Type", /application\/json/);
       const helperResult = await helper.getTopFiveScoreUsers();
+
       assert.strictEqual(result.body.length, 5);
       assert.deepStrictEqual(result.body.map(u => delete u.id), helperResult.map(u => delete u.password));
+    });
+  });
+
+  describe("adding new users", () => {
+    test("a new user with valid values can be added", async () => {
+      const user = {
+        username: "Dr Peixes",
+        password: "fishingislife",
+      };
+
+      const newUser = await api
+        .post("/api/users")
+        .send(user)
+        .expect(201)
+        .expect("Content-Type", /application\/json/);
+      const dbAfter = await helper.usersInDb();
+      assert.strictEqual(dbAfter.length, helper.initialUsers.length+1);
+            
+      user.score = 0;
+      delete user.password;
+      delete newUser.body.id;
+      assert.deepStrictEqual(newUser.body, user);
     });
   });
 });
