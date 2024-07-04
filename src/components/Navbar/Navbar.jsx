67eb64efe5b33ca034d034f2daf848cdb37da236
Wr3tchedTorch <ericmoura511+git@gmail.com>
2024-07-04 16:52:30 -0300

    fixed some responsiveness bugs

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -14,7 +14,12 @@ const Navbar = () => {
             <a href="https://github.com/Wr3tchedTorch" target="_blank" className="github">    <img src="/social/github.svg" alt="github icon" />    </a>
         </div>
         <img src="/logo.svg" alt="" className="logo" />
-        <div className={`links ${menuState ? "collapse-open" : "collapse-closed"}`}>
+        <div className={`links collapsed ${menuState ? "swing-in-top-fwd" : "swing-out-top-bck"}`}>
+            <div className="social social-collapsed">
+              <a href="https://www.instagram.com/ericericeri_/" target="_blank" className="instagram"> <img src="/social/instagram.svg" alt="instagram icon" /> </a>
+              <a href="https://www.linkedin.com/in/eric-moura-368b4724b/" target="_blank" className="linkedin">  <img src="/social/linkedin.svg" alt="linkedin icon" />  </a>
+              <a href="https://github.com/Wr3tchedTorch" target="_blank" className="github">    <img src="/social/github.svg" alt="github icon" />    </a>
+            </div>
             <a href="" className="nav-link active"><p>Play</p></a>
             <a href="" className="nav-link"><p>Leaderboard</p></a>
             <a href="" className="nav-link"><p>Help</p></a>
--- src/components/SettingsMenu/SettingsMenu.jsx
+++ src/components/SettingsMenu/SettingsMenu.jsx
@@ -1,9 +1,32 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import Switch from '../Switch/Switch';
 import "./index.scss";
+import usersService from '../../services/usersService';
+import { Link } from 'react-router-dom';
 
 const SettingsMenu = () => {
+  const [username, setUsername] = useState("");
+  const [isLogged, setIsLogged] = useState(false);
   const [settingsMenuOpen, setSettingsMenuOpen] = useState(false);
+
+  useEffect(() => {
+    let token = localStorage.getItem("user-token");
+    if (token === null) return;
+
+    usersService.validateToken(token).then(result => {
+      if (result) {
+        setIsLogged(true);
+        setUsername(localStorage.getItem("username"));
+      }
+    })
+  }, [])
+
+  const signOut = (e) => {
+    e.preventDefault();
+    localStorage.removeItem("username");
+    localStorage.removeItem("user-token");    
+    window.location.reload();
+  }
 
   return (
     <div className={settingsMenuOpen ? "settings-container-open" : "settings-container-closed"}>
@@ -17,10 +40,16 @@ const SettingsMenu = () => {
           <div className="switch-group">
               <p>Disable Animations</p> <Switch />
           </div>
+          {isLogged ? 
           <div className="button-group">
-              <button>Login</button>
-              <button>Sign Up</button>
-          </div>
+            <p>{username}.</p>
+            <button onClick={signOut}>Sign out</button>
+          </div> :
+          <div className="button-group">
+              <Link to={"/login"}><button>Login</button></Link>
+              <Link to={"/signUp"}><button>Sign up</button></Link>
+          </div>          
+        }
         </form>
         <div className="settings-btn" onClick={() => setSettingsMenuOpen(!settingsMenuOpen)}>
           <img src="/gear.svg" alt=""  className="gear"/>
